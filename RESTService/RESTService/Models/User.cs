5298126827591ebf29f8628e70371c6054e1b6d0
btran333 <btran333@gmail.com>
2020-08-23 16:11:48 -0700

    RESTService Project2

---
+++ RESTService/RESTService/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RESTService.Models
+{
+    public class User
+    {
+        [JsonProperty("ID")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("Email")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [JsonProperty("Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [JsonProperty("DateAdded")]
+
+        public DateTime DateAdded { get; set; }
+    }
+
+}
